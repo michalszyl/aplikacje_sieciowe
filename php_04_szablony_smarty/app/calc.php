47e5fd010f0cb6c4c85ada1b06058d7f7ac5cf68
michalszyl <michal0szyl@gmail.com>
2021-11-06 17:20:19 +0100

    Zadanie4

---
+++ php_04_szablony_smarty/app/calc.php
@@ -0,0 +1,88 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+//załaduj Smarty
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
+
+//pobranie parametrów
+function getParams(&$form){
+	$form['x'] = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$form['y'] = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$form['z'] = isset($_REQUEST['z']) ? $_REQUEST['z'] : null;	
+}
+
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$form,&$infos,&$msgs,&$hide_intro){
+
+	//sprawdzenie, czy parametry zostały przekazane - jeśli nie to zakończ walidację
+	if ( ! (isset($form['x']) && isset($form['y']) && isset($form['z']) ))	return false;	
+	
+	//parametry przekazane zatem
+	//nie pokazuj wstępu strony gdy tryb obliczeń (aby nie trzeba było przesuwać)
+	// - ta zmienna zostanie użyta w widoku aby nie wyświetlać całego bloku itro z tłem 
+	$hide_intro = true;
+
+	$infos [] = 'Przekazano parametry.';
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $form['x'] == "") $msgs [] = 'Nie podano liczby 1';
+	if ( $form['y'] == "") $msgs [] = 'Nie podano liczby 2';
+	if ( $form['z'] == "") $msgs [] = 'Nie podano liczby 3';
+	
+	//nie ma sensu walidować dalej gdy brak parametrów
+	if ( count($msgs)==0 ) {
+		// sprawdzenie, czy $x i $y są liczbami całkowitymi
+		if (! is_numeric( $form['x'] )) $msgs [] = 'Pierwsza wartość nie jest liczbą';
+		if (! is_numeric( $form['y'] )) $msgs [] = 'Druga wartość nie jest liczbą';
+		if (! is_numeric( $form['z'] )) $msgs [] = 'Trzecia wartość nie jest liczbą';
+	}
+	
+	if (count($msgs)>0) return false;
+	else return true;
+}
+	
+// wykonaj obliczenia
+function process(&$form,&$infos,&$msgs,&$result){
+	$infos [] = 'Parametry poprawne. Wykonuję obliczenia.';
+	
+	//konwersja parametrów na int
+	$form['x'] = floatval($form['x']);
+	$form['y'] = floatval($form['y']);
+	$form['z'] = floatval($form['z']);
+	
+	//wykonanie operacji
+	$result = round(($form['x'] + ($form['x'] * $form['y'] / 100)) / ($form['z'] * 12));
+}
+
+//inicjacja zmiennych
+$form = null;
+$infos = array();
+$messages = array();
+$result = null;
+$hide_intro = false;
+	
+getParams($form);
+if ( validate($form,$infos,$messages,$hide_intro) ){
+	process($form,$infos,$messages,$result);
+}
+
+// 4. Przygotowanie danych dla szablonu
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Przykład 04');
+$smarty->assign('page_description','Profesjonalne szablonowanie oparte na bibliotece Smarty');
+$smarty->assign('page_header','Szablony Smarty');
+
+$smarty->assign('hide_intro',$hide_intro);
+
+//pozostałe zmienne niekoniecznie muszą istnieć, dlatego sprawdzamy aby nie otrzymać ostrzeżenia
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+// 5. Wywołanie szablonu
+$smarty->display(_ROOT_PATH.'/app/calc.html');
---
+++ php_04_szablony_smarty/app/templates_c/e1d04ff6f87b60f2513a692aec8ab8863cec4d5d_0.file.calc.html.php
@@ -0,0 +1,146 @@
+<?php
+/* Smarty version 3.1.33, created on 2021-11-06 17:14:19
+  from 'C:\xampp\htdocs\php_04_szablony_smarty\app\calc.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_6186a9dba89fb8_25120279',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e1d04ff6f87b60f2513a692aec8ab8863cec4d5d' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_04_szablony_smarty\\app\\calc.html',
+      1 => 1636214758,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6186a9dba89fb8_25120279 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_17474359546186a9dba73204_38709416', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_986047646186a9dba74067_08481543', 'content');
+$_smarty_tpl->inheritance->endChild($_smarty_tpl, "../templates/main.html");
+}
+/* {block 'footer'} */
+class Block_17474359546186a9dba73204_38709416 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_17474359546186a9dba73204_38709416',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_986047646186a9dba74067_08481543 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_986047646186a9dba74067_08481543',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2 class="content-head is-center">Kalkulator kredytowy</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/app/calc.php" method="post">
+		<fieldset>
+
+			<label for="x">Kwota:</label>
+			<input id="x" type="text" placeholder="wartość x" name="x" value="<?php echo $_smarty_tpl->tpl_vars['form']->value['x'];?>
+">
+					
+			<label for="y">Orocentowanie:</label>
+			<input id="y" type="text" placeholder="wartość y" name="y" value="<?php echo $_smarty_tpl->tpl_vars['form']->value['y'];?>
+">
+
+			<label for="z">Ile lat:</label>
+			<input id="z" type="text" placeholder="wartość z" name="z" value="<?php echo $_smarty_tpl->tpl_vars['form']->value['z'];?>
+">
+
+			<button type="submit" class="pure-button">Oblicz</button>
+		</fieldset>
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+
+<?php if (isset($_smarty_tpl->tpl_vars['messages']->value)) {?>
+	<?php if (count($_smarty_tpl->tpl_vars['messages']->value) > 0) {?> 
+		<h4>Wystąpiły błędy: </h4>
+		<ol class="err">
+		<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['messages']->value, 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+		<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+		<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['infos']->value)) {?>
+	<?php if (count($_smarty_tpl->tpl_vars['infos']->value) > 0) {?> 
+		<h4>Informacje: </h4>
+		<ol class="inf">
+		<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['infos']->value, 'msg');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
+?>
+		<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+</li>
+		<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['result']->value)) {?>
+	<h4>Wynik</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['result']->value;?>
+
+	</p>
+<?php }?>
+
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
